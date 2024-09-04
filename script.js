114313f8066b88a0fe9496aceb4a7850873b6e5e
Gustavo <gustzzs.007@gmail.com>
2024-09-04 11:06:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,12 @@ function calcular(event) {
     var imc = peso / (altura * altura);
 
     // Obtém o elemento para exibir o resultado
-    var resultado = document.getElementById('resultado');{
+    var resultado = document.getElementById('resultado'); // Remove a chave extra aqui
 
     // Classifica e exibe o resultado com base no IMC calculado
-    if (imc < 16) {
+    if (isNaN(imc)) {
+        resultado.innerHTML = `<br> Por favor, insira valores válidos.`;
+    } else if (imc < 16) {
         resultado.innerHTML = `<br> Seu resultado foi: ${imc.toFixed(3)}<br> Cuidado, você está com baixo peso muito grave!`;
     } else if (imc >= 16 && imc < 17) {
         resultado.innerHTML = `<br> Seu resultado foi: ${imc.toFixed(3)}<br> Cuidado, você está com baixo peso grave!`;
@@ -29,5 +31,4 @@ function calcular(event) {
     } else if (imc >= 40) {
         resultado.innerHTML = `<br> Seu resultado foi: ${imc.toFixed(3)}<br> Cuidado, você está com obesidade grau III (mórbida)!`;
     }
-}
 }
